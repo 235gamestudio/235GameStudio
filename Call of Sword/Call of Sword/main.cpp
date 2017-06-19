9fffe3ac35da803761596e5490cc1e8f93b203e3
Jakub <Jakub@DESKTOP-95OAMSP>
2017-06-19 22:36:38 +0200

    TODO/Working on changes

--- Call of Sword/Call of Sword/main.cpp
+++ Call of Sword/Call of Sword/main.cpp
@@ -10,11 +10,15 @@
 /* - repair bug with running animation (f.e. if you keep pressing 2 keys it should stop animate)
 /* - rendering present hp/mana percentage while holding mouse cursor on orbs
 /* - add changing minimap size (button event)
-/* - create fog of war
+/* - create fog of war (maybe)
 /* - delete all tmp's
 /* - repair player movement speed - it should move per second, not per pixel
 /**********************************************************************/
-
+/*						WORKING ON
+/* - full compatibilty with Tiled Map Editor (right now: sth like 50%~)
+/* - collisions using boost library (geometry) - it depends on Tiled Map Editor (right now: 5%)
+/**********************************************************************/
+s
 //#define BOOST_LIB_DIAGNOSTIC
 
 int main(int argc, char *argv[])
